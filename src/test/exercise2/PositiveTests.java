78c64852c1a29f9389594f0749edd7b1232a1dfd
Davud <dav.murtazin@gmail.com>
2016-09-27 19:03:02 +0300

    test updated

--- src/test/exercise2/PositiveTests.java
+++ src/test/exercise2/PositiveTests.java
@@ -1,17 +1,47 @@
 package exercise2;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Davud_Murtazin on 9/27/2016.
  */
+@RunWith(Parameterized.class)
 public class PositiveTests {
+    private double x;
+    private double y;
+    Coordinate coordinate;
+
+    public PositiveTests(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 0, 0 },
+                { 1, 1 },
+                { 2, 1 },
+                { 3, 2 },
+                { 4, 3 },
+                { 5, 5 },
+                { 5, 4}
+        });
+    }
 
     @Test
     public void checkCoordinates() throws Exception {
-
+        coordinate = new Coordinate(x,y);
+        Assert.assertEquals(true, CoordinateUtil.checkCoordinates(coordinate));
     }
-
 }
